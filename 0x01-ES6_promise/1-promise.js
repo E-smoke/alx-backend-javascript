d3f751d0fcca3b6459341c69c0f7177436496691
Elijah <eachala5@gmail.com>
2024-07-01 19:15:41 +0000

    1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
-export default function getFullResponseFromAPI(success){
-  let promise = new Promise((resolve, reject) => {
-    if (success){
-      resolve({status: 200, body: 'Success'});
-    }
-    else{
-      reject("The fake API is not working currently");
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' }); // Use single quotes for 'Success'
+    } else {
+      reject('The fake API is not working currently'); // Use single quotes consistently
     }
   });
   return promise;
